3c91d2238b7a8788ef4ebcdc8c645d7d621e9006
Aleksey Shilo <chief_exb@mail.ru>
2019-06-09 22:45:44 +0300

    Add config support to main.py

--- robocms_app/main.py
+++ robocms_app/main.py
@@ -7,18 +7,19 @@ from aiohttp_security import setup as setup_security
 from aiohttp_security import SessionIdentityPolicy
 from aiopg.sa import create_engine
 from aioredis import create_pool
-
+from settings import config
 
 from db_auth import DBAuthorizationPolicy
 from handlers import Web
 
 
 async def init(loop):
+
     redis_pool = await create_pool(('localhost', 6379))
-    db_engine = await create_engine(user='aiohttp_security',
-                                    password='aiohttp_security',
-                                    database='aiohttp_security',
-                                    host='127.0.0.1')
+    db_engine = await create_engine(user= config['postgres']['user'],
+                                    password=config['postgres']['password'],
+                                    database=config['postgres']['database'],
+                                    host=config['postgres']['host'])
     app = web.Application()
     app.db_engine = db_engine
     setup_session(app, RedisStorage(redis_pool))
